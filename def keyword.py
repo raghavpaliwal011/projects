e5c90a072b959d65d171cf6179c43576e80a9976
raghav <raghavpaliwal1109@gmail.com>
2024-03-15 16:42:50 +0530

    done

---
+++ def keyword.py
@@ -0,0 +1,17 @@
+def process(a,b):
+    add = (a+b)
+    print (add)
+a = 2
+b = 3
+process(a,b)
+if (a>b):
+    print ("a is greater than b")
+else:
+    print ("b is greater than c")
+c = 4
+d = 5
+process(c,d)
+if (b>c):
+    print("b is greater than c")
+else:
+    print ("c is greater than b")
